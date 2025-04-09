213b27dd4f12564e907ac65d4348378afd04ad9c
whomoonlight <158584712+whomoonlight@users.noreply.github.com>
2025-04-09 12:04:05 +0300

    second commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    
+  </header>
+
+  <main>
+    <div style="color:red">
+
+    </div>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
